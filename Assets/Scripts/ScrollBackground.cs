f7db5dac09b6b3eeb28269bfa280c2256a046082
Jade Viana <45906157+jadeviana@users.noreply.github.com>
2020-03-20 18:38:56 -0300

    Scrolling background added

---
+++ Assets/Scripts/ScrollBackground.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollBackground : MonoBehaviour
+{
+    //Script adapted from "Unity 2D Infinite Scrolling Background (For Endless Runner Games) - 2018" by Charger Games on Youtube.
+    private Material backgroundMaterial;
+    private Vector2 offset;
+    [SerializeField] private float xSpeed;
+
+    private void Awake() {
+        backgroundMaterial = GetComponent<Renderer>().material;
+    }
+
+    private void Start()
+    {
+        offset = new Vector2(xSpeed,0);
+    }
+
+    private void Update()
+    {
+        backgroundMaterial.mainTextureOffset += offset*Time.deltaTime;
+    }
+}
