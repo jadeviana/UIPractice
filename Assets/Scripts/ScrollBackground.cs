8a7ab18b67038a0b1d465406fd738185589ee766
Jade Viana <45906157+jadeviana@users.noreply.github.com>
2020-03-25 15:31:24 -0300

    FlexibleLayoutGroup script added

--- Assets/Scripts/ScrollBackground.cs
+++ Assets/Scripts/ScrollBackground.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class ScrollBackground : MonoBehaviour
 {
-    //Script adapted from "Unity 2D Infinite Scrolling Background (For Endless Runner Games) - 2018" by Charger Games on Youtube.
+    //Script from "Unity 2D Infinite Scrolling Background (For Endless Runner Games) - 2018" by Charger Games on Youtube.
     private Material backgroundMaterial;
     private Vector2 offset;
     [SerializeField] private float xSpeed;
 
-    private void Awake() {
+    private void Awake() 
+    {
         backgroundMaterial = GetComponent<Renderer>().material;
     }
 
