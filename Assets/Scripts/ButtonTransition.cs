c697b86c1fe02dad3a5e39b6bf756e464c7a772d
Jade Viana <45906157+jadeviana@users.noreply.github.com>
2020-03-30 12:13:55 -0300

    Button transition script fixed
    Button transition script fixed and new background materials added

--- Assets/Scripts/ButtonTransition.cs
+++ Assets/Scripts/ButtonTransition.cs
@@ -3,26 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using TMPro;
 
 public class ButtonTransition : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 {
     [SerializeField] private RectTransform buttonContent = null;
+    [SerializeField] private Image buttonIcon = null;
+    [SerializeField] private TextMeshProUGUI buttonTitle = null;
+    private Vector2 initialPosition;
+    private float initialPosY;
+
 
     private void Start()
     {
-        buttonContent = GetComponent<RectTransform>();
-        var yPos = buttonContent.anchoredPosition.y;
+        initialPosition = buttonContent.localPosition;
+        initialPosY = buttonContent.localPosition.y;
     }
+
 
     public void OnPointerDown(PointerEventData eventData)
     {
-        //buttonContent.anchoredPosition = new Vector2(0, 8);
-        Debug.Log("Pressed");
+        buttonIcon.color = new Color32(70, 70, 70, 125);
+        buttonTitle.color = new Color32(70, 70, 70, 125);
+        buttonContent.localPosition = new Vector2(0, initialPosY - 8);
     }
 
+    //Return Button Visual to initial state
     public void OnPointerUp(PointerEventData eventData)
     {
-        //buttonContent.anchoredPosition = new Vector2(0, 0);
-        Debug.Log("Released");
+        buttonIcon.color = new Color32(255, 255, 255, 255);
+        buttonTitle.color = new Color32(255, 255, 255, 255);
+        buttonContent.localPosition = initialPosition;
     }
 }
