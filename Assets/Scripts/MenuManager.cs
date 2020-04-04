bb13d1f4cfa347f78e415d30652f3cccabc3b63c
Jade Viana <45906157+jadeviana@users.noreply.github.com>
2020-04-04 14:01:39 -0300

    UI Manager added

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+using TMPro;
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField] private GameObject mainMenu = null;
+    [SerializeField] private RectTransform ribbonTitle = null;
+    [SerializeField] private RectTransform buttonPanel = null;
+
+    public void CloseMainMenu()
+    {
+
+        //mainMenu.SetActive(false);
+    }
+    public void OpenMainMenu()
+    {
+        mainMenu.SetActive(true);
+    }
+
+}
