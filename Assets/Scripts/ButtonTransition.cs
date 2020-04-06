d009863e1e89b408c7a1d786b10f0d42da7735ef
Jade Viana <45906157+jadeviana@users.noreply.github.com>
2020-04-06 18:42:00 -0300

    Animations added to menu
    Fade In and Fade out animations added;
    New UI elements added to Pop-Ups;
    New button prefab added;
    A few assets changes.

--- Assets/Scripts/ButtonTransition.cs
+++ Assets/Scripts/ButtonTransition.cs
@@ -8,8 +8,6 @@ using TMPro;
 public class ButtonTransition : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 {
     [SerializeField] private RectTransform buttonContent = null;
-    [SerializeField] private Image buttonIcon = null;
-    [SerializeField] private TextMeshProUGUI buttonTitle = null;
     private Vector2 initialPosition;
     private float initialPosY;
 
@@ -23,16 +21,12 @@ public class ButtonTransition : MonoBehaviour, IPointerDownHandler, IPointerUpHa
 
     public void OnPointerDown(PointerEventData eventData)
     {
-        //buttonIcon.color = new Color32(70, 70, 70, 125);
-        //buttonTitle.color = new Color32(70, 70, 70, 125);
         buttonContent.localPosition = new Vector2(0, initialPosY - 8);
     }
 
     //Return Button Visual to initial state
     public void OnPointerUp(PointerEventData eventData)
     {
-        //buttonIcon.color = new Color32(255, 255, 255, 255);
-        //buttonTitle.color = new Color32(255, 255, 255, 255);
         buttonContent.localPosition = initialPosition;
     }
 }
--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -7,17 +7,48 @@ using TMPro;
 
 public class MenuManager : MonoBehaviour
 {
-    [SerializeField] private GameObject mainMenu = null;
-    [SerializeField] private RectTransform ribbonTitle = null;
-    [SerializeField] private RectTransform buttonPanel = null;
+    [SerializeField] private GameObject MenuScreen = null;
+    [SerializeField] private GameObject ribbonTitle = null;
+    [SerializeField] private GameObject buttonPanel = null;
 
-    public void CloseMainMenu()
+    private CanvasGroup titleCvGroup;
+    private CanvasGroup btnPanelCvGroup;
+    private float animDuration = 0.5f;
+
+    private void Start()
     {
-        //mainMenu.SetActive(false);
+        titleCvGroup = ribbonTitle.GetComponent<CanvasGroup>();
+        titleCvGroup.alpha = 0;
+
+        btnPanelCvGroup = buttonPanel.GetComponent<CanvasGroup>();
+        btnPanelCvGroup.alpha = 0;
+
+        FadeIn(animDuration);
     }
-    public void OpenMainMenu()
+
+    public void FadeIn(float time)
     {
-        mainMenu.SetActive(true);
+        titleCvGroup.DOFade(1.0f, time);
+        btnPanelCvGroup.DOFade(1.0f, time);
+    }
+    public IEnumerator FadeOut(float time)
+    {
+        titleCvGroup.DOFade(0f, time);
+        btnPanelCvGroup.DOFade(0f, time);
+
+        yield return new WaitForSeconds(time);
+
+        MenuScreen.SetActive(false);
+    }
+
+    public void CloseMenu()
+    {
+        StartCoroutine(FadeOut(animDuration));
+    }
+    public void OpenMenu()
+    {
+        MenuScreen.SetActive(true);
+        FadeIn(animDuration);
     }
 
 }
